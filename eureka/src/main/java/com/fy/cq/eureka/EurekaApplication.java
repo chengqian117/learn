4e701ffce8fc7f4eff865edf7a6496eafeb1a822
chengqian117 <18234124127@163.com>
2019-11-26 15:44:45 +0800

    瞎搞

---
+++ eureka/src/main/java/com/fy/cq/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.fy.cq.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
