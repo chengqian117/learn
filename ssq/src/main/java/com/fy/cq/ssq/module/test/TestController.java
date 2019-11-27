18899168a19148c4a89481f0270b5fa3c4c7df0f
chengqian117 <18234124127@163.com>
2019-11-27 16:36:17 +0800

    测试

--- ssq/src/main/java/com/fy/cq/ssq/module/test/TestController.java
+++ ssq/src/main/java/com/fy/cq/ssq/module/test/TestController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Component
 @RequestMapping("test")
 @Profile("test")
 @RefreshScope
